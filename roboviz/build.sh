24698603b3bac22ff283314230e0a8a173ffb523
Sander van Dijk <sgvandijk@gmail.com>
2018-10-06 16:41:47 +0100

    Build script for roboviz

---
+++ roboviz/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Building default version in Dockerfile and tagging as 'latest'"
+    ARGS=
+    TAG=latest
+else
+    echo "Building and tagging version: $1"
+    ARGS="--build-arg ROBOVIZ_RELEASE=$1"
+    TAG=$1
+fi
+
+docker build $ARGS -t rcss3d/roboviz:$TAG .
