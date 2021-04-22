f7f5e2c6ed75eac4f2d0b82e706c729c287cf7b1
Parth Jindal <pmjindal@gmail.com>
2021-04-23 04:08:37 +0530

    add xauth cookies

---
+++ install_nvidia.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+###### INSTALL DOCKER & DOCKER-COMPOSE #######
+
+##install docker-ce
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \ 
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+## docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+## install nvidia-container toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-docker2
+systemctl restart docker
---
+++ run_nvidia.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# generate xauth cookies
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth nmerge -
+
+while test $# -gt 0; do
+    case "$1" in
+    -h | --help)
+        echo "options:"
+        echo "-h, --help             show brief help"
+        echo "-a, --agent_port       specify agent_port"
+        echo "-s, --server_port      specify server_port"
+        echo "--server               If set,only starts rcssserver"
+        echo "--monitor              If set,only starts monitor"
+        exit 0
+        ;;
+    -a | --agent_port)
+        shift
+        AGENT_PORT=$1
+        ;;
+    -s | --server_port)
+        shift
+        SERVER_PORT=$1
+        ;;
+    --server)
+        service="$service rcssserver3d"
+        ;;
+    --monitor)
+        service="$service roboviz"
+        ;;
+    --*)
+        echo "bad option $1"
+        ;;
+    esac
+    shift
+done
+
+#set default vals
+SERVER_PORT=${SERVER_PORT:-3200}         #default: 3200
+AGENT_PORT=${AGENT_PORT:-3100}           #default: 3100
+service=${service:-rcssserver3d roboviz} #default: all-services
+
+export SERVER_PORT
+export AGENT_PORT
+
+docker-compose -f docker-compose.nvidia.yml up $service
